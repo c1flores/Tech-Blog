200b169962899395a175de253f3d218f59ff12d7
Christian Flores <c1flores@ucsd.edu>
2022-11-22 10:19:58 -0800

    Second attempt to add post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,11 +4,55 @@ const { Post, Comment, User } = require("../../models/");
 const withAuth = require("../../utils/auth");
 
 //Post route for making post
+router.post("/", withAuth, (req, res) => {
+    const body = req.body;
+    console.log(req.session.userId);
+    Post.create({ ...body, userId: req.session.userId })
+        .then(newPost => {
+            res.json(newPost);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
-//Post route for updating post
+//Put route for updating post
+router.put("/:id", withAuth, (req, res) => {
+    Post.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(affectedRows => {
+            if (affectedRows > 0) {
+                res.status(200).end();
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
-//Post route for deleting post
-
+//Delete route for deleting post
+router.delete("/:id", withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(affectedRows => {
+            if (affectedRows > 0) {
+                res.status(200).end();
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
 
 module.exports = router;
