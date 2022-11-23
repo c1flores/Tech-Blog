8efd3d5f187ee85950c7ebd9e2eaee8ac69d513a
Christian Flores <c1flores@ucsd.edu>
2022-11-23 06:55:29 -0800

    Final changes before fixing user experience

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,8 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api/');
-const homeRoutes = require('./home-routes');
-const dashboardRoutes = require('./dashboard-routes');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 //Create modular route handlers
 router.use('/', homeRoutes);
--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,33 +1,26 @@
 //Comment form logic
-async function commentFormHandler(event) {
+const commentFormHandler = async function (event) {
     event.preventDefault();
 
-    const comment_text = document.querySelector('input[name="comment-body"]').value.trim();
+    const postId = document.querySelector('input[name="post-id"]').value;
+    const body = document.querySelector('textarea[name="comment-body"]').value;
 
-    const post_id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
-
-    if (comment_text) {
-        const response = await fetch('/api/comments', {
+    if (body) {
+        await fetch('/api/comment', {
             method: 'POST',
             body: JSON.stringify({
-                post_id,
-                comment_text
+                postId,
+                body
             }),
             headers: {
                 'Content-Type': 'application/json'
             }
         });
 
-        if (response.ok) {
-            document.location.reload();
-
-        } else {
-            alert(response.statusText);
-            document.querySelector('#comment-form').style.display = "block";
-        }
+        document.location.reload();
     }
-}
+};
 
-document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+document
+    .querySelector('#new-comment-form')
+    .addEventListener('submit', commentFormHandler);
